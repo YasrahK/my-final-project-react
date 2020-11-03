260fb32292b5feb008613aa8283360ee7e73b97d
Yasrah Kapadia <yasrah.kapadiaa@gmail.com>
2020-11-03 09:54:04 -0600

    Added git-hub link to the project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    
+    <h1> Hello World </h1>
+    <footer>
+      Project coded by 
+      <a href="https://www.linkedin.com/in/yasrah-kapadia-12841196/" target="_blank"> Yasrah Kapadia </a>and is open-sourced on
+      <a href="https://github.com/YasrahK/my-final-project-react" target="_blank"> Git-hub</a>
+    </footer>
     </div>
   );
 }
