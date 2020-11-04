21aa8846e21af941a5a603f2af0979e3fdc6cb6f
Yasrah Kapadia <yasrah.kapadiaa@gmail.com>
2020-11-04 09:01:47 -0600

    Updated the footer links on App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <h1> Hello World </h1>
     <footer>
       Project coded by 
-      <a href="https://www.linkedin.com/in/yasrah-kapadia-12841196/" target="_blank"> Yasrah Kapadia </a>and is open-sourced on
-      <a href="https://github.com/YasrahK/my-final-project-react" target="_blank"> Git-hub</a>
+      <a href="https://www.linkedin.com/in/yasrah-kapadia-12841196/" target="_blank" rel="noopener noreferrer"> Yasrah Kapadia </a>and is open-sourced on
+      <a href="https://github.com/YasrahK/my-final-project-react" target="_blank" rel="noopener noreferrer"> Git-hub</a>
     </footer>
     </div>
   );
