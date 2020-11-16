6d9e6bf838ba985ecc375d284ff4d96d3e8c0a93
Yasrah Kapadia <yasrah.kapadiaa@gmail.com>
2020-11-16 00:22:37 -0600

    added bootstrap to the index.js file

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
     <h1> Hello World </h1>
     <footer>
       Project coded by 
       <a href="https://www.linkedin.com/in/yasrah-kapadia-12841196/" target="_blank" rel="noopener noreferrer"> Yasrah Kapadia </a>and is open-sourced on
       <a href="https://github.com/YasrahK/my-final-project-react" target="_blank" rel="noopener noreferrer"> Git-hub</a>
     </footer>
+    </div>
     </div>
   );
 }
