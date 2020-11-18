12d99b2a921b1372cd1c7a9b971bc6244cc10a40
Yasrah Kapadia <yasrah.kapadiaa@gmail.com>
2020-11-18 00:02:26 -0600

    Added css styling to the Weather.js file

--- src/Weather.js
+++ src/Weather.js
@@ -3,19 +3,38 @@ import React from 'react';
 export default function Weather() {
     return (
    <div className="Weather">
+     <form className="search-engine">
+       <div className="row">
+         <div className="col-6">
+       <input 
+       type="search"
+       placeholder="Enter a city..."
+       className="form-control"
+       autoFocus="on"
+       />
+       </div>
+       <div className="col-3">
+        <input type="button" value="search" className="btn btn-primary w-100" /></div>
+        <div className="col-3">
+        <input type="button" value="📍" className="btn btn-primary w-100" /></div>
+        </div>
+     </form>
      <h1> Chicago </h1>
      <div className="CurrentWeather">
         <div className="row">
-          <div className="col-4">
-            <i className="fas fa-sun"></i>
+          <div className="col-6">
+            <div className="clear-fix">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy"
+            className="float-left" />
           </div>
-          <div className="col-4">
+          </div>
+            <div className="float-left">
             <span className="TempToday"> 55 </span>
-            <span className="cel-fahren">
-              <button className="TempC">°C</button ><button className="TempF">°F</button >
+            <span className="unit">°C|°F
             </span>
-          </div>
-          <div className="col-4">
+            </div>
+        
+          <div className="col-6">
             <p>
               {" "}
               Humidity: 9% <br /> Wind: 9km/hr <br /> Feels like: 45{" "}
